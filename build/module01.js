b6a4825eb1173a9d1fb4f24d4039c96b619541c8
julio <julioroja987@gmail.com>
2022-11-10 13:15:28 -0300

    aprendiendo los types basicos

--- build/module01.js
+++ build/module01.js
@@ -1,5 +1,44 @@
 "use strict";
-function addNumbers(x, y) {
-    return x + y;
+let randomValue = 10;
+randomValue = "algo";
+randomValue = true;
+//console.log(randomValue());
+// Asercion de tipos, existen 2 formatos
+//(randomValue as string).toUpperCase();
+//(<string>randomValue).toUpperCase();
+if (typeof randomValue == "string") {
+    console.log(randomValue.toUpperCase());
 }
-console.log(addNumbers(3, 6));
+else {
+    console.log("Se esperaba un string");
+}
+// Tipos de union
+// el tipo any no tiene validaciones, podemos usar estos tipos de union para limitar las opciones
+let multiType;
+multiType = 20; // valido
+multiType = true; // valido
+multiType = "twenty"; // invalido
+function add(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        console.log(x + y);
+        return;
+    }
+    if (typeof x === "string" && typeof y === "string") {
+        console.log(x.concat(" " + y));
+        return;
+    }
+    // throw new Error("Los parametros deben ser numeros o strings");
+}
+add(2, 4); // retorna 6
+add("Hola", "usuario"); // retorna "Hola usuario"
+// Creo un nuevo manager del tipo ManagementEmployee que combina las interfaces dado que el manager tambien es empleado
+let newManager = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true,
+};
+console.log(newManager);
+let myResult;
+myResult = "pass"; // Valido
+myResult = "incomplete"; // Valido
+myResult = "failure"; // Invalido
--- module01.ts
+++ module01.ts
@@ -1,4 +1,71 @@
-function addNumbers(x: number, y: number) {
-    return x + y;
+let randomValue: unknown = 10;
+randomValue = "algo";
+randomValue = true;
+
+//console.log(randomValue());
+
+// Asercion de tipos, existen 2 formatos
+//(randomValue as string).toUpperCase();
+//(<string>randomValue).toUpperCase();
+
+if (typeof randomValue == "string") {
+    console.log((randomValue as string).toUpperCase());
+} else {
+    console.log("Se esperaba un string");
 }
-console.log(addNumbers(3, 6));
+
+// Tipos de union
+// el tipo any no tiene validaciones, podemos usar estos tipos de union para limitar las opciones
+
+let multiType: number | boolean;
+multiType = 20; // valido
+multiType = true; // valido
+multiType = "twenty"; // invalido
+
+function add(x: number | string, y: number | string) {
+    if (typeof x === "number" && typeof y === "number") {
+        console.log(x + y);
+        return;
+    }
+    if (typeof x === "string" && typeof y === "string") {
+        console.log(x.concat(" " + y));
+        return;
+    }
+    // throw new Error("Los parametros deben ser numeros o strings");
+}
+
+add(2, 4); // retorna 6
+add("Hola", "usuario"); // retorna "Hola usuario"
+// add("one", 2); // retorna error
+
+// Tipo de interseccion
+// Similar a la union pero es inclusiva
+
+//Se definen interfaces
+interface Employee {
+    employeeID: number;
+    age: number;
+}
+
+interface Manager {
+    stockPlan: boolean;
+}
+
+// Defino el tipo ManagementEmployee
+type ManagementEmployee = Employee & Manager;
+
+// Creo un nuevo manager del tipo ManagementEmployee que combina las interfaces dado que el manager tambien es empleado
+let newManager: ManagementEmployee = {
+    employeeID: 12345,
+    age: 34,
+    stockPlan: true,
+};
+
+console.log(newManager);
+
+// Tipos literales, Objetos, matrices, funciones
+type testResult = "pass" | "fail" | "incomplete"; // esto es un string pero es limitado, dado que un string es infinitas posibilidades
+let myResult: testResult;
+myResult = "pass"; // Valido
+myResult = "incomplete"; // Valido
+myResult = "failure"; // Invalido
